25a84eb1238209a38db75a5a75c9922247495faa
Manu Rodriguez <ebmanu.r1999@gmail.com>
2022-01-06 19:31:05 -0300

    prueba de las importaciones de los modulos correcta

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 /* Importaciones de modulos */
 import {user} from './js/clase/user.js'
-import {factura} from './js/clase/facturas.js'
+//import {factura} from './js/clase/facturas.js'
+import { registroUser } from './js/Funcionalidades/registroCliente.js'
+
+let usuario1 = new user("manu", 123, 155);
+registroUser(usuario1);
---
+++ js/Funcionalidades/registroCliente.js
@@ -0,0 +1,5 @@
+import {user} from  '../clase/user.js';
+
+export const registroUser = (cliente) =>{
+    console.log(cliente.getId())
+}
