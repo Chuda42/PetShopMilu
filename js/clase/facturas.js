3745979975784542668da229701fcebfadda4bd6
Manu Rodriguez <ebmanu.r1999@gmail.com>
2022-01-10 11:28:36 -0300

    facturas actualizado

--- js/clase/facturas.js
+++ js/clase/facturas.js
@@ -9,41 +9,96 @@ export class factura{
         this.producto   = producto; //lista de objeto tipo producto
         this.monto      = monto;    //int
         this.metPago    = metPago;  //String 
-        this.fecha      = fecha.toLocaleDateString();   //fecha es de tipo Date(); el metodo utilizado devuelve la fecha actual como string
-        this.finalizada = false;    //booleano que indica si la factura se puede acutalizar, false si, true no;
+        this.fecha      = fecha.toLocaleDateString();   //fecha tipo Date(), atributo tipo string
+        this.finalizada = false;    //booleano
     }
 
-    /*Retorna el usuario registrado en la factura*/
+    /*METODO RETORNO DE PROPIEDADES */
+    //user
     getUser() {
         return this.user;
     }
 
-    /*Cambia el usuario registrado en la factura*/
-    setUser(user) {
-        this.user = user; //objeto tipo user
-    }
-
-    /*Retorna la lista de productos*/
+    //producto
     getProducto() {
-        return this.producto; //lista de objeto tipo producto
+        return this.producto;
     }
 
-    /*Agrega a la lista de productos un nuevo objeto tipo producto*/
-    agregarProducto(producto){
-        this.producto.push(producto); //agrega a la lista de productos un nuevo objeto tipo producto
+    //monto
+    getMonto(){
+        return this.monto;
     }
 
-    /*Actualizar fecha*/
-    updateDate(fecha){
-        this.fecha = fecha.toLocaleDateString();
+    //metPago
+    getModoPago(){
+        return this.metPago;
+    }
+
+    //fecha
+    getFecha(){
+        return this.fecha;
+    }
+
+    //estado de finalización
+    getEstado(){
+        return this.finalizada;
+    }
+
+
+    /* METODOS DE CAMBIO DE VALORES SI this.finalizada == false */
+    //cambia titular factura
+    setUser(user) {
+        if (!this.finalizada)       
+            this.user = user;       
+    }
+
+    //agrega un nuevo producto a la lista this.producto
+    agregarProducto(producto){            
+        if(!this.finalizada)
+            this.producto.push(producto); 
+    }
+
+    //cambia valor de this.monto por monto
+    udateMonto(monto){
+        if(!this.finalizada)
+            this.monto = monto;
     }
     
-    /*Imprime la información factura por consola*/
-    imprimirFactura(){
-
+    //cambia valor de this.metPago por metodo
+    selectModoPago(metodo){
+        if(!this.finalizada)
+            this.metPago = metodo;
     }
 
-    /*cambia el estado de this.finalizada a true*/
+    //cambia this.fecha por fecha.toLocalDateString()
+    updateDate(fecha){
+        if(!this.finalizada)                         
+            this.fecha = fecha.toLocaleDateString();
+    }
+    
+    /* METODOS DE IMPRESION DE INFORMACIÓN */
+    //muestra la información de la factura mediante alert()
+    imprimirFactura(){
+        if(this.finalizada){
+            alert( `Titular de la factura: ${this.user}\n
+                    Lista de productos: ${this.producto.join('-')}\n
+                    Monto de la compra: ${this.monto}\n
+                    Metodo de pago: ${this.metPago}\n
+                    Fecha de facturación: ${this.fecha}`)
+        }else
+            alert("La factura aún no esta finalizada")
+    }
+
+    
+    /* METODOS DE COMPARACIÓN */
+    //retorna true si el this.monto es mayor a factura.monto
+    esMayorMonto(factura){ //factura tipo factura
+        return this.monto > factura.getMonto();
+    }
+
+
+    /* METODO DE FINALIZACIÓN */
+    //cambia el estado de this.finalizada a true
     finalizarFactura(){
         this.finalizada = true;
     }
@@ -51,4 +106,4 @@ export class factura{
 
 
 
-}
+}//class
