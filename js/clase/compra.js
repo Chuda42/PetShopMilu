c23022594ca9740252ab480d1a6a81ed07137faf
Manu Rodriguez <ebmanu.r1999@gmail.com>
2022-01-10 18:10:48 -0300

    se modifica compra.js

--- js/clase/compra.js
+++ js/clase/compra.js
@@ -3,8 +3,26 @@
 */
 
 export class compra{
-    constructor(){
-        
+    constructor(user){
+        this.user = user;     //tipo user
+        this.productos = [];        //tipo lista de productos
+        this.monto = 0;             //int, monto de la compra
+    }
+
+    /* METODOS RETORNO DE PROPIEDADES  */
+    //retorna user 
+    getUser() {
+        return this.user;
+    }
+
+    //retorna this.productos lista de productos
+    getProductos(){
+        return this.productos;
+    }
+
+    //retorna this.monto, monto total de la compra
+    getMonto(){
+        return this.monto;
     }
 
 }//class
