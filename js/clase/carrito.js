7935d886419629935a6d1e603fd47572fa1ed52b
Manu Rodriguez <ebmanu.r1999@gmail.com>
2022-01-10 18:25:17 -0300

    modificado carrito.js

--- js/clase/carrito.js
+++ js/clase/carrito.js
@@ -9,4 +9,34 @@ export class carrito{
         this.monto = 0;         //int, representa monto total de la compra
         this.id = id;           //int que identifica a la compra
     }
+
+    /* METODOS DE RETORNO DE PROPIEDADES*/
+    //retorna el user titular del carrito
+    getUser(){
+        return this.user;
+    }
+
+    //retorna la lista de productos this.product
+    getProduct(){
+        return this.product;
+    }
+
+    //retorna el monto de la compra this.monto
+    getMonto(){
+        return this.monto;
+    }
+
+    //retorna el id de la compra this.id
+    getId(){
+        return this.id;
+    }
+
+
+    /* METODOS DE ACUTALIZACIÓN DE PROPIEDADES */
+    //agregar producto, agrega un elemento tipo producto a this.product, y suma el monto al total
+    agregarProducto(producto){
+        this.product.push(producto);
+        this.monto += producto.getPrice();
+    }
+    
 }//class
