5fff8690eb6efa786d1da4e063ad2300bec1bcae
Manu Rodriguez <ebmanu.r1999@gmail.com>
2022-01-10 12:09:22 -0300

    Update user

--- js/clase/user.js
+++ js/clase/user.js
@@ -7,72 +7,83 @@ export class user{
         this.name      = userName; //string
         this.pass      = pass;     //string  
         this.id        = id;       //int
-        this.billetera = 0;        //int, indica los fondos de la billetera cuando recien se instancia es 0      
-        this.historial = [];       //Lista de facturaciónes del cliente
-        this.active    = false;    //indica si el usuario esta logeado
+        this.billetera = 0;        //int, saldo de la billetera   
+        this.historial = [];       //Lista de tipo factura
+        this.active    = false;    //indica si el usuario esta logeado (booleano)
 
     }
 
 
     /*ESTADO DE ACTIVIDAD*/
-    /*Pasa el estado del objeto a activo*/
+    //cambia this.active a true
     userLogin() {
         this.active = true;
     }
-    /*Pasa el estado del objeto a inactivo*/
+
+    //cambia this.active a false
     userLogout() {
         this.active = false;
     }
 
+
+
     /*OBTENER INFORMACIÓN DEL USER*/
-    /*retorna el nombre*/
+    //retorna this.name (username)
     getName() {
         return this.name;
     }
 
-    /*retorna el id*/
+    //retorna el id del cliente
     getId() {
         return this.id;
     }
 
-    /*retorna el saldo de la billetera*/
+    //retorna el saldo de la billetera, this.billetera
     getSaldo(){
         return this.billetera;
     }
 
+    //retorna el this.historial, historial del user
+    getHistorial(){
+        return this.historial;
+    }
+
+
+
     /*CAMBIA INFORMACIÓN DEL USUARIO*/
-    
-    /*suma a la cantidad actual de la billetera monto*/
+    //cambia this.nome por el valor de name
+    udateName(name){
+        this.name = name;
+    }
+
+    //cambia this.pass por el valor de pass
+    udatePass(pass){
+        this.pass = pass;
+    }
+
+    //a this.billetera (saldo) le suma monto 
     sumarSaldo(monto){
         this.billetera = this.billetera + monto; 
     }
 
-    /*resta monto a la cantidad actual de la billetara, si el resultado es menor a 0 manda una
-    alerta de saldo insuficiente*/
+    //a this.billetera le resta monto si el resultado, es negativo manda alert de saldo insuficiente
     restarSaldo(monto){
         let res = this.billetera - monto;
         (res < 0)? alert(`Saldo disponible ${this.billetera}`) : this.billetera = res;
     }
 
-    /*actualizar saldo de billetera en monto*/
+    //cambia el this.monto por el valor de monto
     udateSaldo(monto){
         this.billetera = monto;
     }
     
-    /*actualizar nombre de usuario*/
-    udateName(name){
-        this.name = name;
-    }
-
-    /*actualiza el pass del usuario*/
-    udatePass(pass){
-        this.pass = pass;
-    }
-
-    /*agrega un nuevo objeto factura al historial*/
+    //agrega un nuevo objeto factura a this.historial
     pushFactura(factura){
         this.historial.push(factura);
     }
-}
+
+    /* Puedo agregar realizarComprar()
+    NO LA TENGO BIEN CLARA TODAVIA */
+}//class
 
 
